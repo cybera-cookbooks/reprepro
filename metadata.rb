dd5ba3d79423fb3547665159b38441c53e1b4725
Cameron Mann <camthemann@gmail.com>
2013-10-01 13:56:40 -0600

    Updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             "reprepro"
-maintainer       "Opscode"
-maintainer_email "cookbooks@opscode.com"
+maintainer       "Cybera"
+maintainer_email "devops@cybera.ca"
 license          "Apache 2.0"
 description      "Installs/Configures reprepro for an apt repository"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.1"
+version          "0.4"
 
 # Doesn't make sense to indicate support for non Debian platforms!
 supports "ubuntu"
